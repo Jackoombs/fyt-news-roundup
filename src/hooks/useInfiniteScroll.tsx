ee48eea457c26586ec6075dcf84e3403e9092454
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-28 20:04:56 +0100

    Added a custom hook to tell if the user has reached the bottom of the page.

---
+++ src/hooks/useInfiniteScroll.tsx
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+export function useInfiniteScroll() {
+  const [isBottom, setIsBottom] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      const scrollHeight = document.documentElement.scrollHeight;
+      const clientHeight = document.documentElement.clientHeight;
+      if (scrollTop + clientHeight >= scrollHeight - 50) {
+        setIsBottom(true);
+      } else {
+        setIsBottom(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return isBottom;
+}
