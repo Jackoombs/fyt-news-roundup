147fdffc54b30fa33868417f9405f90a7bb64be2
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-31 00:27:34 +0100

    Added loading and error components.

--- src/components/Article/ArticleGrid.tsx
+++ src/components/Article/ArticleGrid.tsx
@@ -5,6 +5,7 @@ import { useInfiniteScroll } from "../../hooks/useInfiniteScroll";
 import type { ArticleListInput } from "../../types/trpc";
 import { Article } from "./Article";
 import { trpc } from "../../utils/trpc";
+import { Loading } from "../ui/Loading";
 
 interface Props {
   query: ArticleListInput;
@@ -17,18 +18,42 @@ export const ArticleGrid = ({ query }: Props) => {
     data: articles,
     isLoading,
     error,
+    refetch,
   } = trpc.article.list.useQuery(query);
 
-  if (isLoading) return <h1>Loading...</h1>;
-  if (error || !articles) return <h1>Error</h1>;
+  if (isLoading) return <Loading />;
+  if (error || !articles)
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center gap-2 pb-4 text-center md:gap-8">
+        <h1 className="text-2xl font-semibold md:text-4xl">
+          Failed to fetch articles
+        </h1>
+        <button
+          className="w-max rounded bg-indigo-600 px-6 py-1 font-bold text-indigo-50"
+          onClick={() => refetch()}
+        >
+          Try again
+        </button>
+      </div>
+    );
 
   const activeArticle = articles[activeArticleIndex];
+
+  if (articles.length === 0) {
+    return (
+      <div className="flex h-full w-full flex-col items-center justify-center">
+        <h1 className="max-w-xl text-center text-2xl font-semibold leading-[3] md:text-4xl">
+          No articles found, try adding some new categorys on the Outlets page
+        </h1>
+      </div>
+    );
+  }
 
   return (
     <>
       <AnimatePresence mode="wait" initial={true}>
         {activeArticleIndex < 0 && (
-          <motion.div
+          <motion.ul
             key={0}
             initial={{ opacity: 0, y: 200 }}
             animate={{ opacity: 1, y: 0 }}
@@ -38,7 +63,7 @@ export const ArticleGrid = ({ query }: Props) => {
           >
             {articles.map((article, index) => (
               <ArticleCard
-                key={article.link}
+                key={article.id}
                 {...{
                   article,
                   index,
@@ -48,7 +73,7 @@ export const ArticleGrid = ({ query }: Props) => {
                 }}
               />
             ))}
-          </motion.div>
+          </motion.ul>
         )}
 
         {activeArticleIndex >= 0 && activeArticle && (
