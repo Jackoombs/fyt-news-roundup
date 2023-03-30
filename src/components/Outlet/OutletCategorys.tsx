b6d0c5a3422f9c1e25232b586074b383e9be0728
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-03-31 00:26:36 +0100

    Added loading component spinner.

--- src/components/Outlet/OutletCategorys.tsx
+++ src/components/Outlet/OutletCategorys.tsx
@@ -1,5 +1,6 @@
 import { OutletListOutput } from "../../types/trpc";
 import { trpc } from "../../utils/trpc";
+import { Loading } from "../ui/Loading";
 import { OutletCategoryCard } from "./OutletCategoryCard";
 
 interface Props {
@@ -11,18 +12,22 @@ export const OutletCategorys = ({ activeOutlet }: Props) => {
     outletId: activeOutlet.id,
   });
 
+  if (isLoading) return <Loading />;
+
   return (
     <div className="mx-auto max-w-4xl">
       <div className="flex flex-col gap-2 pb-2">
         <p className="pb-4 text-lg font-semibold">
           Activate the news pages you would like to receive articles for.
         </p>
-        {data?.map((category, index) => (
-          <OutletCategoryCard
-            key={category.url}
-            {...{ category, index, activeOutlet }}
-          />
-        ))}
+        <ul className="flex flex-col gap-1">
+          {data?.map((category, index) => (
+            <OutletCategoryCard
+              key={category.url}
+              {...{ category, index, activeOutlet }}
+            />
+          ))}
+        </ul>
       </div>
     </div>
   );
