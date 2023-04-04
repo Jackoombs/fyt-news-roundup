a5132cfe9ab21d9e7fff5aafb30fa958e12ba827
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-04-04 22:35:09 +0100

    Updated card css

--- src/components/Article/ArticleCard.tsx
+++ src/components/Article/ArticleCard.tsx
@@ -19,44 +19,42 @@ export const ArticleCard = ({
 }: Props) => {
   return (
     <motion.li
-      className="flex"
+      className="flex flex-col justify-between rounded-lg bg-gradient-to-tr from-indigo-500 to-indigo-800 p-8 pb-4 text-indigo-50"
       initial={{ y: 20, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5, delay: index * 0.05, ease: "easeInOut" }}
     >
-      <a className="flex flex-col justify-between rounded-lg bg-gradient-to-tr from-indigo-500 to-indigo-800 p-8 pb-4 text-indigo-50">
-        <div className="flex flex-col gap-1 pb-3">
-          <h3 className="text-left text-xl font-semibold line-clamp-2">
-            {article.title}
-          </h3>
-          <p className="text-left text-sm font-medium leading-6 line-clamp-3 ">
-            {article.summary}
+      <div className="flex flex-col gap-1 pb-3">
+        <h3 className="text-left text-xl font-semibold line-clamp-2">
+          {article.title}
+        </h3>
+        <p className="text-left text-sm font-medium leading-6 line-clamp-3 ">
+          {article.summary}
+        </p>
+      </div>
+      <div className="flex w-full justify-between">
+        <div className="flex w-max flex-col text-left">
+          <p className="text-md rounded font-bold tracking-widest">
+            {article.category}
           </p>
-        </div>
-        <div className="flex w-full justify-between">
-          <div className="flex w-max flex-col text-left">
-            <p className="text-md rounded font-bold tracking-widest">
-              {article.category}
+          {article.date && (
+            <p className="rounded text-xs font-bold">
+              {format(article.date, "dd/MM/yyyy")}
             </p>
-            {article.date && (
-              <p className="rounded text-xs font-bold">
-                {format(article.date, "dd/MM/yyyy ")}
-              </p>
-            )}
-          </div>
-          <ArticleSaveButton
-            saved={article.saved}
-            link={article.link}
-            query={query}
-          />
+          )}
         </div>
-        <button
-          onClick={() => setActiveArticleIndex(index)}
-          className="w-max self-center rounded-lg px-2 py-1 text-sm font-semibold tracking-widest underline underline-offset-4 duration-200 hover:text-indigo-900"
-        >
-          view article
-        </button>
-      </a>
+        <ArticleSaveButton
+          saved={article.saved}
+          link={article.link}
+          query={query}
+        />
+      </div>
+      <button
+        onClick={() => setActiveArticleIndex(index)}
+        className="w-max self-center rounded-lg px-2 py-1 text-sm font-semibold tracking-widest underline underline-offset-4 duration-200 hover:text-indigo-900"
+      >
+        view article
+      </button>
     </motion.li>
   );
 };
