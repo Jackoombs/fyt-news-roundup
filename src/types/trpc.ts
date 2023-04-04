90c3ff58f722a4af0d1d69bcd67ac3dfc5003192
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-04-04 22:35:49 +0100

    Added additional procedure types.

--- src/types/trpc.ts
+++ src/types/trpc.ts
@@ -4,7 +4,9 @@ import type { AppRouter } from "../server/trpc/router/_app";
 type RouterInput = inferRouterInputs<AppRouter>;
 type RouterOutput = inferRouterOutputs<AppRouter>;
 
-export type OutletGetOutput = NonNullable<RouterOutput["outlet"]["get"]>;
 export type ArticleListInput = RouterInput["article"]["list"];
+export type ArticleCreateInput = RouterInput["article"]["create"];
+
+export type OutletGetOutput = NonNullable<RouterOutput["outlet"]["get"]>;
 export type OutletListOutput = RouterOutput["outlet"]["list"];
 export type ArticleListOutput = RouterOutput["article"]["list"];
