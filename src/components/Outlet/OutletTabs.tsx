81a4091bd560ab41771d1dce04585dad069e67a2
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-10 12:49:42 +0100

    Abstracted tab to reuseable component.

--- src/components/Outlet/OutletTabs.tsx
+++ src/components/Outlet/OutletTabs.tsx
@@ -1,14 +1,14 @@
-import { Tab } from "../../pages/outlets";
-import clsx from "clsx";
-import { motion } from "framer-motion";
+import type { TabType } from "../../types/types";
+import { Tab } from "../ui/Tab";
+import type { Tabs } from "../../pages/outlets";
 
 interface Props {
-  activeTab: Tab["value"];
-  setActiveTab: React.Dispatch<React.SetStateAction<Tab["value"]>>;
+  activeTab: Tabs;
+  setActiveTab: React.Dispatch<React.SetStateAction<Tabs>>;
 }
 
 export const OutletTabs = ({ activeTab, setActiveTab }: Props) => {
-  const tabTemplate: Tab[] = [
+  const tabTemplate: TabType<Tabs>[] = [
     {
       name: "Articles",
       value: "ARTICLES",
@@ -22,22 +22,8 @@ export const OutletTabs = ({ activeTab, setActiveTab }: Props) => {
   return (
     <div>
       <ul className="grid w-max grid-cols-2">
-        {tabTemplate.map(({ name, value }) => (
-          <li role="tab" key={value} className="relative">
-            <button
-              className="relative z-10 w-40 px-6 py-2 font-semibold"
-              onClick={() => setActiveTab(value)}
-            >
-              {name}
-            </button>
-            {value === activeTab && (
-              <motion.div
-                transition={{ duration: 0.3 }}
-                layoutId="tab"
-                className="absolute left-0 top-0 h-full w-full rounded-t-lg bg-indigo-300"
-              />
-            )}
-          </li>
+        {tabTemplate.map((tab) => (
+          <Tab key={tab.value} {...{ tab, activeTab, setActiveTab }} />
         ))}
       </ul>
     </div>
