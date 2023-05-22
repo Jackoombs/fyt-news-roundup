de5837302e867272ea535a83825fdbaec5cb9430
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-22 11:53:05 +0100

    Changed page to layout.

--- src/app/outlets/[outletName]/layout.tsx
+++ src/app/outlets/[outletName]/layout.tsx
@@ -3,8 +3,16 @@ import { getClient } from "@/lib/client";
 import { Outlets } from "@/types/graphql";
 import DashboardHeader from "@/components/ui/DashboardHeader";
 import OutletSelect from "@/components/Outlet/OutletSelect";
+import { DashboardMenu } from "@/components/ui/DashboardMenu";
+import DashboardMenuItem from "@/components/ui/DashboardMenu/DashboardMenuItem";
+import path from "path";
 
-const Page = async ({ params }: { params: { outletName: string } }) => {
+interface Props {
+  children: React.ReactNode;
+  params: { outletName: string };
+}
+
+const Layout = async ({ params, children }: Props) => {
   const apollo = getClient();
 
   let outlets = await apollo.query<Outlets>({
@@ -28,17 +36,28 @@ const Page = async ({ params }: { params: { outletName: string } }) => {
     `,
   });
 
+  const currentPath = path.join("outlets", params.outletName);
+
   return (
     <>
-      <div className="flex justify-between">
+      <div className="flex justify-between pb-6">
         <DashboardHeader>Outlets</DashboardHeader>
         <OutletSelect
           currentOutlet={params.outletName}
           outlets={outlets.data}
         />
       </div>
+      <DashboardMenu>
+        <DashboardMenuItem link={path.join(currentPath, "overview")}>
+          Overview
+        </DashboardMenuItem>
+        <DashboardMenuItem link={path.join(currentPath, "categories")}>
+          Categories
+        </DashboardMenuItem>
+      </DashboardMenu>
+      <div className="">{children}</div>
     </>
   );
 };
 
-export default Page;
+export default Layout;
