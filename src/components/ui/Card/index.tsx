b14b20c8a9b97ab910a5be81b356ef057f0c13b3
Jackoombs <jack.coombs@hotmail.co.uk>
2023-05-24 12:21:47 +0100

    Added a card component to wrap dashboard elements.

---
+++ src/components/ui/Card/index.tsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+
+interface Props {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Card = ({ children, className }: Props) => {
+  return (
+    <div
+      className={clsx(
+        "bg-white dark:bg-slate-950 ring-1 ring-slate-200 dark:ring-slate-800 p-6 rounded-lg",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+interface CardHeaderProps {
+  children: React.ReactNode;
+  size?: "text-sm" | "text-md" | "text-lg";
+}
+
+const CardHeader = ({ children, size = "text-sm" }: CardHeaderProps) => {
+  return (
+    <h3
+      className={clsx(
+        "text-slate-900 font-bold line-clamp-2 mb-2 dark:text-slate-200",
+        size
+      )}
+    >
+      {children}
+    </h3>
+  );
+};
+
+export { Card, CardHeader };
