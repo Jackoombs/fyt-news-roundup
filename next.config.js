d4cacd1754044fb2a45f730074e802271ff87e4c
Jackoombs <jack.coombs@hotmail.co.uk>
2023-05-24 12:18:37 +0100

    Added swiper and dates-fns packages.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: "/",
-        destination: "/outlets/BBC",
+        destination: "/outlets/BBC/overview",
         permanent: true,
       },
     ];
