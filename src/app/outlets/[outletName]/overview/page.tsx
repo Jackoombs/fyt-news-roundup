1e2c1a2ff598b820ad80e36aa2b2c186c75f3276
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 09:36:50 +0100

    Fixed wrong method applied to params.

--- src/app/outlets/[outletName]/overview/page.tsx
+++ src/app/outlets/[outletName]/overview/page.tsx
@@ -1,23 +1,26 @@
 import RecentArticles from "@/components/Article/RecentArticles";
 import ActiveCategorys from "@/components/Category/ActiveCategorys";
-import DashboardHeader from "@/components/ui/DashboardHeader";
-import DashboardWrapper from "@/components/ui/DashboardWrapper";
-import { removeWhiteSpace } from "@/lib/utils";
+import AnimateWrapper from "@/components/ui/AnimateWrapper";
+import { addWhiteSpace } from "@/lib/utils";
+import path from "path";
 
 interface Props {
   params: { outletName: string };
 }
 
 const Page = ({ params }: Props) => {
+  const articlePagePath = path.join("outlets", params.outletName, "articles");
+
   return (
-    <DashboardWrapper className="grid grid-cols-[minmax(0,1fr)_26rem] grid-rows-[auto_1fr] gap-6">
+    <AnimateWrapper className="grid grid-cols-[minmax(0,1fr)_26rem] grid-rows-[auto_1fr] gap-6">
       {/* @ts-expect-error Server Component */}
       <RecentArticles
         options={{
-          filterBy: { outletName: removeWhiteSpace(params.outletName) },
+          filterBy: { outletName: [addWhiteSpace(params.outletName)] },
           orderBy: [{ field: "date", direction: "desc" }],
           take: 6,
         }}
+        articlePagePath={articlePagePath}
       />
       <span />
       {/* @ts-expect-error Server Component */}
@@ -25,12 +28,12 @@ const Page = ({ params }: Props) => {
         className="w-full"
         options={{
           filterBy: {
-            outletName: removeWhiteSpace(params.outletName),
+            outletName: addWhiteSpace(params.outletName),
             active: true,
           },
         }}
       />
-    </DashboardWrapper>
+    </AnimateWrapper>
   );
 };
 
