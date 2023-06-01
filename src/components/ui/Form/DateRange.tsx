13720517c7c783a0a7ab58002771c7f06c2d142b
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 10:17:43 +0100

    Updated calendar component to no longer rely on state.

--- src/components/ui/Form/DateRange.tsx
+++ src/components/ui/Form/DateRange.tsx
@@ -9,28 +9,66 @@ import {
 } from "@/components/ui/Popover";
 import useRouterUtils from "@/lib/hooks/useRouterUtils";
 import { cn } from "@/lib/utils";
-import { format } from "date-fns";
+import { format, parse } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import { DateRange } from "react-day-picker";
 
 const DatePicker = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
   const routerUtils = useRouterUtils();
+  const searchParams = useSearchParams();
 
   const [date, setDate] = useState<DateRange | undefined>();
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   routerUtils.updateSearchParams([
+  //     {
+  //       key: "startDate",
+  //       value: date?.from ? format(date?.from, "LLLddy") : undefined,
+  //     },
+  //     {
+  //       key: "endDate",
+  //       value: date?.to ? format(date?.to, "LLLddy") : undefined,
+  //     },
+  //   ]);
+  // }, [date, routerUtils]);
+
+  const handleSelect = (e: DateRange | undefined) => {
     routerUtils.updateSearchParams([
       {
         key: "startDate",
-        value: date?.from ? format(date?.from, "LLLddy") : undefined,
+        value: e?.from ? format(e?.from, "LLLddy") : undefined,
       },
       {
         key: "endDate",
-        value: date?.to ? format(date?.to, "LLLddy") : undefined,
+        value: e?.to ? format(e?.to, "LLLddy") : undefined,
       },
     ]);
-  }, [date, routerUtils]);
+  };
+
+  const parseDates = () => {
+    const date: DateRange = { from: undefined, to: undefined };
+
+    if (searchParams.has("startDate")) {
+      date.from = parse(
+        searchParams.get("startDate")!.toString(),
+        "LLLddy",
+        new Date()
+      );
+    }
+
+    if (searchParams.has("endDate")) {
+      date.to = parse(
+        searchParams.get("endDate")!.toString(),
+        "LLLddy",
+        new Date()
+      );
+    }
+    return date;
+  };
+
+  const dates = parseDates();
 
   return (
     <div className={cn("grid gap-2", className)}>
@@ -45,14 +83,14 @@ const DatePicker = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
             )}
           >
             <CalendarIcon className="mr-2 h-4 w-4" />
-            {date?.from ? (
-              date.to ? (
+            {dates?.from ? (
+              dates.to ? (
                 <>
-                  {format(date.from, "LLL dd, y")} -{" "}
-                  {format(date.to, "LLL dd, y")}
+                  {format(dates.from, "LLL dd, y")} -{" "}
+                  {format(dates.to, "LLL dd, y")}
                 </>
               ) : (
-                format(date.from, "LLL dd, y")
+                format(dates.from, "LLL dd, y")
               )
             ) : (
               <span>Pick a date</span>
@@ -63,9 +101,9 @@ const DatePicker = ({ className }: React.HTMLAttributes<HTMLDivElement>) => {
           <Calendar
             initialFocus
             mode="range"
-            defaultMonth={date?.from}
-            selected={date}
-            onSelect={setDate}
+            defaultMonth={dates?.from}
+            selected={parseDates()}
+            onSelect={handleSelect}
             numberOfMonths={2}
           />
         </PopoverContent>
