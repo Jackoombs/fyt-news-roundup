1fda124e24954348cabd8d8a340115b38c2ca4e5
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 09:14:28 +0100

    Added padding prop.

--- src/components/ui/DashboardSubHeader.tsx
+++ src/components/ui/DashboardSubHeader.tsx
@@ -1,10 +1,18 @@
+import clsx from "clsx";
+
 interface Props {
   children: React.ReactNode;
+  padding?: string;
 }
 
-const DashboardSubHeader = ({ children }: Props) => {
+const DashboardSubHeader = ({ children, padding = "pb-2" }: Props) => {
   return (
-    <h2 className="font-display font-bold text-xl text-slate-800 dark:text-slate-200 pb-2">
+    <h2
+      className={clsx(
+        "font-display font-bold text-xl text-slate-800 dark:text-slate-200",
+        padding
+      )}
+    >
       {children}
     </h2>
   );
