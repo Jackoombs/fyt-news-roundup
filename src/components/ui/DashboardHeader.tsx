c31c5f16328ea589d1a9f1084c84ad0e99b0f2cd
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 09:14:40 +0100

    Added classname prop.

--- src/components/ui/DashboardHeader.tsx
+++ src/components/ui/DashboardHeader.tsx
@@ -1,10 +1,18 @@
+import clsx from "clsx";
+
 interface Props {
   children: React.ReactNode;
+  className?: string;
 }
 
-const DashboardHeader = ({ children }: Props) => {
+const DashboardHeader = ({ children, className }: Props) => {
   return (
-    <h1 className="font-display font-bold text-4xl text-slate-800 dark:text-slate-200">
+    <h1
+      className={clsx(
+        "font-display font-bold text-4xl text-primary",
+        className
+      )}
+    >
       {children}
     </h1>
   );
