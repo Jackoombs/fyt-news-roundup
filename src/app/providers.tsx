9762c1e5690c5d2a0b4dbe103f39d75262026881
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-01 09:03:01 +0100

    Added client side apollo provider.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,6 +1,40 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  SuspenseCache,
+} from "@apollo/client";
+import {
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from "@apollo/experimental-nextjs-app-support/ssr";
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: "http://localhost:8080/graphql",
+  });
+
+  return new ApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === "undefined"
+        ? ApolloLink.from([
+            new SSRMultipartLink({
+              stripDefer: true,
+            }),
+            httpLink,
+          ])
+        : httpLink,
+  });
+}
+
+function makeSuspenseCache() {
+  return new SuspenseCache();
+}
 
 interface Props {
   children: React.ReactNode;
@@ -9,7 +43,12 @@ interface Props {
 export const Providers = ({ children }: Props) => {
   return (
     <ThemeProvider attribute="class" enableSystem={false}>
-      {children}
+      <ApolloNextAppProvider
+        makeClient={makeClient}
+        makeSuspenseCache={makeSuspenseCache}
+      >
+        {children}
+      </ApolloNextAppProvider>
     </ThemeProvider>
   );
 };
