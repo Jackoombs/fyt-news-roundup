d4a1cbcd48d128e3d7b9bfd2ef671def8f4daf16
Jackoombs <jack.coombs@hotmail.co.uk>
2023-06-05 14:26:48 +0100

    Grid now takes options and handles the fetching.

--- src/components/Article/ArticleGrid.tsx
+++ src/components/Article/ArticleGrid.tsx
@@ -1,12 +1,42 @@
+import { ArticleThumbnail } from "@/components/Article/ArticleThumbnail";
+import { getClient } from "@/lib/client";
+import { GET_ARTICLES } from "@/lib/queries";
+import { Articles, GetArticlesOptions } from "@/types/graphql";
+
 interface Props {
-  children: React.ReactNode;
+  options: GetArticlesOptions;
+  articlePagePath: string;
 }
 
-const ArticleGrid = ({ children }: Props) => {
+const ArticleGrid = async ({ options, articlePagePath }: Props) => {
+  const apollo = getClient();
+
+  const { data } = await apollo.query<Articles>({
+    query: GET_ARTICLES,
+    variables: options,
+  });
+
+  if (data.articles?.length === 0) {
+    return (
+      <p className="text-center mt-20">
+        No articles found. Try widening your search criteria.
+      </p>
+    );
+  }
   return (
-    <div className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
-      {children}
-    </div>
+    <>
+      {data.articles?.length > 0 && (
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-6">
+          {data.articles.map((article, index) => (
+            <ArticleThumbnail
+              articlePagePath={articlePagePath}
+              key={index}
+              article={article}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
 
